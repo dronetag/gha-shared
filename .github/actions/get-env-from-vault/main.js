cca1b877e9eba953ef7841f80f1cdaa51fb1edb1
Marián Hlaváč <hlavac95@gmail.com>
2024-11-05 20:17:33 +0100

    fix(get-env-from-vault): Remove all package dependencies altogether

--- .github/actions/get-env-from-vault/main.js
+++ .github/actions/get-env-from-vault/main.js
@@ -1,26 +1,31 @@
 const fs = require('fs');
-const core = require('@actions/core');
+const os = require('os');
+
+function setFailed(message) {
+  process.exitCode = 1;
+  process.stdout.write("::error title=Failed to get environment from Vault::" + message + os.EOL);
+}
 
 async function run() {
   try {
     // Inputs from action.yml or environment variables
-    const vaultAddr = core.getInput('vault-addr');
-    const vaultToken = core.getInput('vault-token');
-    const secretPath = core.getInput('secret-path');
+    const vaultAddr = process.env[`INPUT_VAULT-ADDR`];
+    const vaultToken = process.env[`INPUT_VAULT-TOKEN`];
+    const secretPath = process.env[`INPUT_SECRET-PATH`];
 
     // Fetch secret from Vault
-    const url = new URL(vaultAddr, secretPath).toString();
+    const url = new URL(secretPath, vaultAddr).toString();
     const headers = { 'X-Vault-Token': vaultToken };
     const response = await fetch(url, { method: 'GET', headers });
 
     if (!response.ok) {
-      core.setFailed(`Error: Unable to fetch data from Vault. Status code: ${response.status}`);
+      setFailed(`Unable to fetch data from Vault. Status code: ${response.status}`);
       return;
     }
 
     const data = (await response.json()).data.data; // yo dawg
     if (!data) {
-      core.setFailed('Error: No data found at the specified path.');
+      setFailed('No data found at the specified path.');
       return;
     }
 
@@ -30,10 +35,10 @@ async function run() {
       .join('\n') + '\n';
 
     fs.writeFileSync('.env', envContent);
-    core.info(`Created .env file with ${Object.entries(data).length} items`);
+    process.stdout.write(`Created .env file with ${Object.entries(data).length} items` + os.EOL);
 
   } catch (error) {
-    core.setFailed(`Action failed with error: ${error.message}`);
+    setFailed(`Action failed with error: ${error.message}`);
   }
 }
 
